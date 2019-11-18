1bcc837d43d0c841875eba9e994ce62cf6047486
Jodi Tivis <joditivis@gmail.com>
2019-11-18 15:08:54 -0700

    added keyword gift generator

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // ************ Firebase Config ******************
-  //Firebase configuration
+//Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyANy9CUVmdOHWlpvzq6-PlyWJgsNfpVcYw",
   authDomain: "gift-ing.firebaseapp.com",
@@ -38,13 +38,22 @@ $(document).ready(function () {
 
   // run the add person function when the submit button on modal is clicked
   $(document).on("click", ".list-group-item", displayPerson);
-  
 
-  function displayPerson(){
-    if(!$(this).attr("disabled")){
+  // runs the generation of gift ideas based on that persons keyword when that person is clicked
+  $(document).on("click", "#regenerate-keyword", regenerateKeyword);
+
+  // *** working on the new keyword generatior form 
+  function regenerateKeyword() {
+    var newKeyword = $("#new-keyword").val();
+    console.log(newKeyword);
+  }
+
+
+  function displayPerson() {
+    if (!$(this).attr("disabled")) {
       $("#persons-content").show();
       var idx = $(this).attr("person-index");
-      
+
       $("#selected-name").text(data.giftList[idx].name + "'s");
       $("#relationship").text(data.giftList[idx].relationship);
       $("#keyword").text(data.giftList[idx].keyword);
@@ -52,6 +61,9 @@ $(document).ready(function () {
       $("#budget").text(data.giftList[idx].budget);
       $("#selected-name-gif").text(data.giftList[idx].name);
       $("#selected-name-search").text(data.giftList[idx].name);
+
+var terms = data.giftList[idx].keyword;  
+      searchEtsy(terms, "displayideas")
     }
   }
 
@@ -67,7 +79,7 @@ $(document).ready(function () {
     var tmpKeyword = $("#add-keyword").val().trim();
     var tmpPersonality = $("#add-personality").val().trim();
     var tmpBudget = $("#add-budget").val().trim();
-    
+
     // push to the data array and update firebase
     updatedGiftListData(tmpName, tmpRelationship, tmpKeyword, tmpPersonality, tmpBudget);
     writeUserData(id, user, data);
@@ -79,16 +91,20 @@ $(document).ready(function () {
 
   // ************ Esty API ******************
 
-// etsy-api-setup
-// etsy ajax call
-// adds item that was searched for can click the image and go to etsy store
-// if more than four images add a break and start new row
-// currently fetching 16 items
+  // etsy-api-setup
+  // etsy ajax call
+  // adds item that was searched for can click the image and go to etsy store
+  // if more than four images add a break and start new row
+  // currently fetching 16 items
   $('#etsy-search').on('submit', function (event) {
     event.preventDefault();
 
-    api_key = "82wfohi8ezzm1fq3vflms9ul";
+    // using etsy value to generate and display results from the keyword
     terms = $('#etsy-terms').val();
+    searchEtsy(terms, "displayresults")
+  })
+  function searchEtsy(terms, origin) {
+    api_key = "82wfohi8ezzm1fq3vflms9ul";
     etsyURL = "https://openapi.etsy.com/v2/listings/active.js?keywords=" +
       terms + "&limit=16&includes=Images:1&api_key=" + api_key;
 
@@ -101,24 +117,36 @@ $(document).ready(function () {
       success: function (data) {
         console.log(data);
         if (data.ok) {
-          $('#etsy-images').empty();
-          if (data.count > 0) {
-            $.each(data.results, function (i, item) {
-              $("<img id='etsy'/>").attr("src", item.Images[0].url_75x75).appendTo("#etsy-images").wrap(
-                '<a href="' + item.url + '" target="_blank"></a>'
-              );
-            });
-          } else {
-            $('<p>No results.</p>').appendTo('#etsy-images');
+          if (origin === "displayresults") {
+            $('#etsy-images').empty();
+            if (data.count > 0) {
+              $.each(data.results, function (i, item) {
+                $("<img id='etsy'/>").attr("src", item.Images[0].url_75x75).appendTo("#etsy-images").wrap(
+                  '<a href="' + item.url + '" target="_blank"></a>'
+                );
+              });
+            } else {
+              $('<p>No results.</p>').appendTo('#etsy-images');
+            }
           }
-        } else {
-          $('#etsy-images').empty();
-          console.log(data.error);
+          else {
+            // Generated Gift Ideas based on keyword
+            $('#etsy-images-generated').empty();
+            if (data.count > 0) {
+              $.each(data.results, function (i, item) {
+                $("<img id='etsy'/>").attr("src", item.Images[0].url_75x75).appendTo("#etsy-images-generated").wrap(
+                  '<a href="' + item.url + '" target="_blank"></a>'
+                );
+              });
+            } else {
+              $('<p>No results.</p>').appendTo('#etsy-images-generated');
+            }
+          }
         }
       }
     });
     return false;
-  })
+  }
 
   // ************ User Enters Email to "Login" ******************
 
@@ -145,78 +173,80 @@ $(document).ready(function () {
 
     // "value" event listener for FB
     database.ref().on("value", function(snapshot){
-     
+
       // for each key item in the db, get the email and the temp items for each record,
       // if it's a "valid" record, then it will have an email, temp records are setup temporarily
       // to trigger the event listener, but will be removed below 
       for (var key in snapshot.val()){
         var ref = database.ref(key);
         ref.once("value").then(function(snapshot){
-              var tmpEmail = snapshot.val().email;
-              var tmpDelete = snapshot.val().temp;
-              // if the email in the FB record matches what was input, then this is the record we want
-              // so it sets the global data equal to the data record associate with the key - we also store
-              // the key so we know which record to update later 
-              if(tmpEmail === user){
-                data=snapshot.val().data;
-                id=snapshot.key;
-                createButtons();
-                //updatePage(); // placeholder for a function that will update the DOM
-              }
-              // if this is temp record, remove it to keep the database clean
-              if(tmpDelete === "temp"){
-                database.ref(key).remove();
-              }
-              
-          })
-          console.log(user, data, id);
+          var tmpEmail = snapshot.val().email;
+          var tmpDelete = snapshot.val().temp;
+          // if the email in the FB record matches what was input, then this is the record we want
+          // so it sets the global data equal to the data record associate with the key - we also store
+          // the key so we know which record to update later 
+          // if this is temp record, remove it to keep the database clean
+          if (tmpDelete === "temp") {
+            database.ref(key).remove();
+          }
+     if (tmpEmail === user) {
+            data = snapshot.val().data;
+            id = snapshot.key;
+            createButtons(id);
+            //updatePage(); // placeholder for a function that will update the DOM
+          }
+     
+        })
+        console.log(user, data, id);
       }
     })
   });
 
   // ************ Create Buttons for Gift List ******************
 
-    // Function for displaying person buttons
-  function createButtons() {
+  // Function for displaying person buttons
+  function createButtons(id) {
     // Clears the buttons that are on page, to repopulate them
     // (this is necessary otherwise we will have repeat buttons)
     $("#person-buttons").empty();
+    console.log(data);
 
     // Looping through the array of people
     for (var i = 0; i < data.giftList.length; i++) {
 
-        // Then dynamically generating buttons
-        var a = $("<li>");
-        // Adding a class to style the button to bootstrap
-        a.addClass("list-group-item list-group-item-action list-group-item-info");
-        // Adding a data-attribute
-        a.data("toggle", "list");
-        a.attr("person-index", i);
-        // Providing the button's text with the person's name
-        a.text(data.giftList[i].name);
-        // setup an attr of disabled to false (meaning it is in enabled) 
-        a.attr("disabled", false);
+      // Then dynamically generating buttons
+      var a = $("<li>");
+      // Adding a class to style the button to bootstrap
+      a.addClass("list-group-item list-group-item-action list-group-item-info");
+      // Adding a data-attribute
+      a.data("toggle", "list");
+      a.attr("person-index", i);
+      a.attr("db-key", id);
+      // Providing the button's text with the person's name
+      a.text(data.giftList[i].name);
+      // setup an attr of disabled to false (meaning it is in enabled) 
+      a.attr("disabled", false);
 
-        // create a button that will hold the x to remove the word
-        var x = $("<button>");
-        // add a class to pull in the bootstrap close
-        x.addClass("close");
-        // add an x to the element as text
-        x.append("&times;");
-        // give the x an attribute with name - this will delete the proper button when clicked
-        // *** Might need to rethink this attribute ***
-        x.attr("person-name-button", i);
-        // append the x button
-        a.append(x);
+      // create a button that will hold the x to remove the word
+      var x = $("<button>");
+      // add a class to pull in the bootstrap close
+      x.addClass("close");
+      // add an x to the element as text
+      x.append("&times;");
+      // give the x an attribute with name - this will delete the proper button when clicked
+      // *** Might need to rethink this attribute ***
+      x.attr("person-name-button", i);
+      // append the x button
+      a.append(x);
 
-        // Adding the button to the HTML
-        $("#person-buttons").append(a);
+      // Adding the button to the HTML
+      $("#person-buttons").append(a);
     }
   }
 
   // ************ Delete a person on Gift List ******************
 
-    // if the close button is clicked, run this function
+  // if the close button is clicked, run this function
   $(document).on("click", ".close", function(){
     // return the parent element - this is the animal button
     var par = $(event.target).parent();
@@ -239,13 +269,13 @@ $(document).ready(function () {
   // to push to FB
   function updatedGiftListData(name, relationship, keyword, personality, budget){
     data.giftList.push({
-      name: name, 
-      relationship: relationship, 
+      name: name,
+      relationship: relationship,
       keyword: keyword,
       personality: personality,
       budget: budget,
       savedGiftIdea: ""
-    }) 
+    })
   }
 
   // ************ Push Data to Firebase ******************
@@ -267,10 +297,10 @@ $(document).ready(function () {
         email: user,
         data: data
       })
-    }  
+    }
   }
 });
-    
+
 
 
 
@@ -282,7 +312,7 @@ $(document).ready(function () {
 
 //console.log(user, data);
     //var userUID = 1;
-  
+
     // var ref = database.ref('/gif-ting/' + userUID);
     // ref.on('value', function(data) {
     //   //data.key will be like -KPmraap79lz41FpWqLI
@@ -299,7 +329,7 @@ $(document).ready(function () {
     // var userId = lists.indexOf(user)+1;
     // console.log(user);
     // console.log(userId);
-    
+
     // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
     //   console.log(snapshot.val());
     //   console.log(snapshot.val().email);
@@ -329,7 +359,7 @@ $(document).ready(function () {
 
   // var userId = lists.user;
   // console.log(userId);
-  
+
   // return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
   //   console.log(snapshot.val().email);
   //   //var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
@@ -339,7 +369,7 @@ $(document).ready(function () {
   // database.ref().on("value", function(snapshot){
   //   console.log("checking database");
   //   for (var key in snapshot.val()){
-     
+
   //     var ref = database.ref(key);
   //     ref.once("value").then(function(snapshot){
   //           var tmpEmail = snapshot.val().email;
@@ -386,7 +416,7 @@ $(document).ready(function () {
   //   //     var childKey = snapshot.child("name/last").key; // "last"
   //   //   });
   // })
-  
+
   // var ref = database.ref("-Ltp_7u-F_Fojt65372p");
   // ref.once("value").then(function(snapshot){
   //   console.log(snapshot.val());
